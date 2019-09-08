16357df6dedbca1ed503d076ad1b25ce7f6310e4
Babak Sefidgar <babak.sefidgar@gmail.com>
2019-09-08 21:36:51 +0430

    add basic functions

--- extension.js
+++ extension.js
@@ -2,3 +2,16 @@ const St = imports.gi.St;
 const Main = imports.ui.main;
 const Gio = imports.gi.Gio;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+
+function init() {}
+
+
+function enable() {
+
+}
+
+
+function disable() {
+    
+}
