39717f07a421df9aea3d9fbecb2e0f1778d0b172
Babak Sefidgar <babak.sefidgar@gmail.com>
2019-09-11 15:55:01 +0430

    add fuzzy clock prototype and finish it

--- extension.js
+++ extension.js
@@ -11,19 +11,88 @@ var box = null;
 var new_clock = null;
 
 
+FuzzyClock.prototype = {
+    _init: function(){
+        this.label = new St.Label();
+        new_clock.add_child(this.label);
+    },
+  
+    BuildTime: function() {
+        scale = 5
+        adverbs = ['exactly ', 'around ', 'almost ']
+        hourList = [' ONE', ' TWO', ' THREE', ' FOUR', ' FIVE', ' SIX',
+                    ' SEVEN', ' EIGHT', ' NINE', ' TEN', ' ELEVEN', ' TWELVE']
+        minuteList = ['FIVE', 'TEN', 'QUARTER', 'TWENTY', 'TWENTY-FIVE', 'HALF']
+        time = 'It\'s '
+
+        let now = new Date();
+        let hours = now.getHours();
+        let minutes = now.getMinutes();
+
+        //changing to 24hr format.
+        if (hours <= 12) {
+        hours += 12;
+        }
+        
+        dmin = minutes % scale    //used to determine the right adverb.
+
+        //pos is used in minuteList.
+        if (minutes > 30) {
+            pos = Math.round((60 - minutes) / scale)
+        } 
+        else {
+            pos = Math.round(minutes / scale)
+        }
+        //specifing the adverb (almost, exactly, around).
+        if (dmin == 0) {
+            time += adverbs[0]
+            pos -= 1
+        } 
+        else if(dmin <= scale/2){
+            time += adverbs[1]
+            if (minutes < 30)
+                pos -= 1
+        } 
+        else{
+            time += adverbs[2]
+            if (minutes > 30)
+                pos -= 1
+        }
+        //specifing the hour and minute words.
+        if (minutes <= Math.round(scale/2)) {
+            time += hourList[hours - 12 - 1]
+        }    
+        else if (minutes >= 60 - Math.round(scale/2)) {
+            time += hourList[hours - 12 - 1]
+        } 
+        else {
+            if (minutes > 30) {
+                time += minuteList[pos] + ' to' + hourList[hours - 12]
+            } 
+            else {
+                time += minuteList[pos] + ' past' + hourList[hours - 12 - 1]
+            }
+        }
+        this.label.set_text(time);
+        }
+  }
+
+
 function init() {}
 
 
 function enable() {
     orig_clock = date_menu.get_children();
-
+    
+    //new_clock is the box that will be replaced with default clock.
+    //box is the selected clocks prototype.
+    new_clock = new St.BoxLayout();
     box = new FuzzyClock();
 
     date_menu.remove_all_children(orig_clock);
     date_menu.add_child(new_clock);
-    signalID = date_menu.connect("notify::text", box.FuzzyTime);
-    last = box.label.get_text();
-    box.FuzzyTime();
+    signalID = date_menu.connect("notify::text", box.BuildTime);
+    box.BuildTime();
 }
 
 
