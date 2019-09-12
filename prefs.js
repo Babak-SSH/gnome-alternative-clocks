033992bb759d02f10d5206aef3e16fe68c6055d4
Babak Sefidgar <babak.sefidgar@gmail.com>
2019-09-12 15:15:08 +0430

    fix typo

--- prefs.js
+++ prefs.js
@@ -1,16 +1,16 @@
 //gnome imports
 const Me = imports.misc.extensionUtils.getCurrentExtension();
-const Gettext = imports.gettext.domain(DOMAIN);
 const Gtk = imports.gi.Gtk;
 const GObject = imports.gi.GObject;
 const Gio = imports.gi.Gio;
 //js imports
 const DOMAIN = Me.metadata['gettext-domain'];
+const Gettext = imports.gettext.domain(DOMAIN);
 const config = imports.misc.config;
 const _ = Gettext.gettext;
 const FUZZY = 'fuzzy-clock';
 const BINARY = 'binary-clock';
-const MHIN = 'mhin-clcok';
+const MHIN = 'mhin-clock';
 const TUNER = 'time-tuner';
 
 
